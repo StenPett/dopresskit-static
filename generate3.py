3b876a625b3e3e1b32cf062417f5711dae54b490
Sten Jørgen Pettersen <stenjorgenpettersen@gmail.com>
2021-01-25 20:14:47 +0100

    Added console outputs

--- generate3.py
+++ generate3.py
@@ -1,12 +1,29 @@
 import xmltodict
 import json
 
+# Convert xml file to python dict
 
 def xml2dict(src):
     with open(src, "r") as xml_file:
         dict_data = xmltodict.parse(xml_file.read())
     return dict_data
 
-dict_data = xml2dict('data.xml')
-ordered_data = json.loads(json.dumps(dict_data))
-print(ordered_data)
+# Functions that output to console
+
+def blabla_ga():
+    if ga is None:
+        print(">>> Google analytics ID Not found, skipping...\n    Use 'python3 generate3.py <Tracking ID> to activate Google analytics")
+    else:
+        print("Google analytics: Enabled (ID: %s)" % (ga))
+
+def blabla_projects():
+    projects = get_projects()
+    if len(projects) == 0:
+        print( ">>> No project found, skipping...")
+        print( "    Duplicate the '_template' folder, rename it, and edit data.xml")
+        print( "    The project folder name must be lowercase, and spaces by _underscores_.")
+
+# Actual presskit stuff
+now_project = "."
+ga = sys.argv[1] if len(sys.argv) == 2 else None
+blabla_ga() 
